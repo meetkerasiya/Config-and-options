4ae84cc5c9edfcc9c9058bb816b1f8bad12e7fc0
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-02 17:43:09 +0530

    Config and options

---
+++ HomePageConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo
+{
+    public class HomePageConfiguration
+    {
+        public bool EnableGreeting { get; set; }
+        [Required]
+        public string ForeCaseSectionTitle { get; set; } =string.Empty;
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Demo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly HomePageConfiguration _homePageConfiguration;
+
+        public string greeting = "Good Evening!";
+        public string title = "null";
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration, IOptions<HomePageConfiguration> option)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            try
+            {
+                _homePageConfiguration = option.Value;
+            }
+            catch (OptionsValidationException ex)
+            {
+                foreach (var failure in ex.Failures)
+                {
+                    _logger.LogError(failure);
+                }
+            }
+        }
+
+        public void OnGet()
+        {
+            //var feature = new Feature();
+            //_configuration.Bind("Features:HomePage", feature);
+
+            if(_homePageConfiguration.EnableGreeting)
+            {
+                greeting = "Good Morning!!!";
+                title = _homePageConfiguration.ForeCaseSectionTitle;
+            }
+
+            //var homePageFeature = _configuration.GetSection("Features:HomePage");
+            //if (homePageFeature.GetValue<bool>("EnableGreeting"))
+            //{
+            //    greeting = "Good morning!";
+            //    title = homePageFeature["ForeCaseSectionTitle"];
+            //}
+            //if (_configuration.GetValue<bool>("Features:HomePage:EnableGreeting"))
+            //{
+            //    greeting = "Good afternoon!";
+            //}
+        }
+    }
+
+    class Feature
+    {
+        public bool EnableGreeting { get; set; }
+        public string ForeCaseSectionTitle { get; set; }
+    }
+}
